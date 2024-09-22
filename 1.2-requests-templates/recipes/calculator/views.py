8c89a196b10482ffad07225daff0a09a55dd98f3
“Admin” <sysoevserega93@gmail.com>
2024-09-22 15:18:36 +0300

    fix 1.2 recipes

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -37,13 +37,3 @@ def get_recipe(request):
 
     context = {'recipe': recipe}
     return render(request, 'calculator/index.html', context)
-
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
