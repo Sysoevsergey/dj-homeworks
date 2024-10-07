592bfe785e900c579b1a4ec328b0282a8b9f287d
“Admin” <sysoevserega93@gmail.com>
2024-10-07 20:28:51 +0300

    add "orm_migrations"

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
+    data = Book.objects.all()
     context = {}
     return render(request, template, context)
